1b4178b0efcfe7e65a54520f848ad2ce1d5f59d2
Laszlo Harri Nemeth <nlharri@gmail.com>
2019-01-25 11:00:22 +0000

    XCode Project Initialization

---
+++ ReallySimpleNoteTakingApp/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ReallySimpleNoteTakingApp
+//
+//  Created by user913992 on 1/25/19.
+//  Copyright © 2019 nlharri. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
